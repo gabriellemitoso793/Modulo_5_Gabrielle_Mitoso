ad5753ba5a0dcc1964451f404b790eeafc1d41de
gabriellemitoso793 <gabrielle.santos@sou.inteli.edu.br>
2024-04-04 13:28:44 -0300

    ponderada - semana 7

--- Ponderada - Semana 8/main.py
+++ Ponderada - Semana 8/main.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template, request
-from tinydb import TinyDB, Query
-from pydobot import Dobot
+from flask import Flask, render_template, request, redirect
+from tinydb import TinyDB
+
+from pydobot.dobot import Dobot
 
 app = Flask(__name__)
 db = TinyDB('db.json')
 
 try:
-    dobot = Dobot(port='COM3')
+    dobot = Dobot(port='COM8')
     print("Conectado com o robô!")
     robot_connected = True
 except Exception as e:
@@ -41,12 +42,20 @@ def control():
         return render_template('control.html', message="Comando enviado!")
     else:
         return render_template('control.html', message="Robô não conectado!")
-
-@app.route('/logs')
+    
+@app.route('/logs', methods=['POST', 'GET'])
 def logs():
+    if request.method == 'POST':
+        if 'delete_db' in request.form:
+            db.truncate()
+            return redirect('/')
+        elif 'update_log' in request.form:
+            log_id = int(request.form.get('log'))
+            new_command = request.form.get('new_command')
+            db.update({'command': new_command}, doc_ids=[log_id])
+            return redirect('/logs')
     logs = db.all()
     return render_template('logs.html', logs=logs)
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=8000)
-
